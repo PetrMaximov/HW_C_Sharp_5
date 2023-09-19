b43b407f88e5905e9f8dcd21191c5ec83f719669
«Petr» <maxpetrb@mail.ru>
2023-09-19 21:43:08 +0300

    Task36 Сумма элементов, стоящих на нечётных позициях массива

---
+++ Task36/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+int NArray (int N)
+{
+    int[] A = new int[N];
+    int sum = 0;
+    while (N > 0)
+    {
+        A[N-1] = new Random().Next(-10,10);
+        if (((N-1) % 2) != 0 )
+        {
+            sum = sum + A[N-1];
+        }
+        N = N - 1;
+    }
+   
+    var strА = string.Join(" ", A);
+    Console.WriteLine(strА);
+    return sum;
+}
+
+
+int res = NArray(8);
+Console.WriteLine("Cумма элементов, стоящих на нечётных позициях равна = " + res);
+
